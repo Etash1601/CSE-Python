c3137bf6059c03f6a979a7d526fdbb24237cd0a9
Etash Ashwin <128017545+Etash1601@users.noreply.github.com>
2023-08-07 10:12:58 +0530

    Create Concatenate strings.py

---
+++ Concatenate strings.py
@@ -0,0 +1,17 @@
+print("Enter n: ")
+n = int(input())
+a=[]
+b=[]
+c=[]
+print("Enter string 1: ")
+for i in range(n):
+    a.append(input())
+print("Enter string 2: ")
+for i in range(n):
+    b.append(input())
+
+print(a)
+print(b)
+c = a+b
+c.sort()
+print(c)
