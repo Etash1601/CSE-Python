44aba3e264f8efac52658691b54b856d5646ef48
Etash Ashwin <128017545+Etash1601@users.noreply.github.com>
2023-08-17 11:00:23 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -30,5 +30,52 @@ def add(a,b,c=30,d=40):
     avg = tot/3
     return tot,avg
 # returning more than 1 variable is taken as tuple
-print(add(10,20))"""
+print(add(10,20))
+"""
+"""
+l=[]
+n=5
+for i in range(n):
+    l=l+[int(input())] # l.append(int(input())
 
+def list(k):
+    k.sort()
+    avg = sum(k)/len(k)
+    return (k,avg)
+
+d,average = list(l)
+print(d)
+print(average)
+
+# more than 1 type of variable and unknown length
+def func(*t):   *t=tuple
+def func2(**t):  **t=dictionary
+
+def add(a,*t)
+add(10,20,30)    # *t=(20,30)
+"""
+"""
+# Recursive factorial
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))
+
+# Recursive Greatest Common Divisor
+def gcd(a,b):
+    if a%b==0:
+        return b
+    else:
+        return gcd(b,a%b)
+    
+print(gcd(100,60))
+
+# Lambda Function
+x = lambda a: a+10
+print(x(5))
+
+Maxnum = lambda a,b: a if(a>b) else b
+print(Maxnum(5,4))
+"""
