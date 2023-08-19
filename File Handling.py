845d99d798f25de827db0093b56a1aad32b4c306
Etash Ashwin <128017545+Etash1601@users.noreply.github.com>
2023-08-19 10:47:13 +0530

    Update File Handling.py

--- File Handling.py
+++ File Handling.py
@@ -1,6 +1,15 @@
 # procedure to open a file
-
+# 1)
 f1 = open("note.txt","r")
 print("Name of the file: ",f1.name)
 print("Closed or not: ",f1.closed)
 print("Opening mode: ",f1.mode)
+
+# 2)
+f = open("filesample.txt","r")
+fcontent = f.read()
+print(len(fcontent))
+f.seek(0) # to again read from the start
+flines = f.readlines()
+print(len(flines))
+f.close
