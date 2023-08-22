0ec0c62d032e6ae2e061ea6bfb266a8e1b6c770e
Etash Ashwin <128017545+Etash1601@users.noreply.github.com>
2023-08-22 08:29:22 +0530

    Create Capitalize first letter.py

---
+++ Capitalize first letter.py
@@ -0,0 +1,9 @@
+""" Write a program to capitalize the first letter of each word in a text.
+Example Input/Output 1:
+Input:
+Have a great day.
+Output:
+Have A Great Day """
+
+string = input("Enter the text: ")
+print(string.title()) # title() method capitalizes first letter of all the words
