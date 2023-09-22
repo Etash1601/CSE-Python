a21eea15037e17c4e35b8030ec5c3e7968df4d38
Etash Ashwin <128017545+Etash1601@users.noreply.github.com>
2023-09-22 10:56:47 +0530

    Create Date_Time.py

---
+++ Date_Time.py
@@ -0,0 +1,46 @@
+# Date_Time
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+pd.date_range(start='2/2/2019', end='02/08/2019')
+print(pd.date_range(start='2/2/2019', end='02/08/2019'))
+# freq=D means day-wise
+# pd.date_range(start='2/2/2019', end='02/08/2019', freq='H') Hour-wise
+
+pd.date_range(start='2/2/2019', periods=8) # no. of days = 8
+
+# Convert string to datetime
+d = {'date': ['3/10/2000','3/11/2000','3/12/2000'], 'value': [2,3,4]}
+df = pd.DataFrame(d)
+print(df)
+df['date'] = pd.to_datetime(df['date'])
+print(df)
+
+# Find the age
+df = pd.DataFrame({'name': ['Tom','Andy','Lucas','Pranav','Uma','Rahu','Kumar','Varun'],
+                   'dob': ['08-05-1997','04-28-1996','12-16-1995','11-16-1998','06-28-1999','04-26-1997','07-09-2000','11-08-2001']})
+df['dob'] = pd.to_datetime(df['dob'])
+df
+
+df['year'] = df['dob'].dt.year
+df['month'] = df['dob'].dt.month
+df['day'] = df['dob'].dt.day
+df
+
+today = pd.to_datetime('today')
+df['age'] = today.year - df['dob'].dt.year
+print(df)
+
+#
+df = df.set_index(['dob'])
+print(df)
+
+
+# select data with specific year and perform aggregation
+df.loc['1997']
+df.loc['1997','age'].sum()
+
+cond = df.index.month == 11
+df[cond]  # only prints for 11th month
